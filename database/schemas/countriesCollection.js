7980748a2a17aa67fc7640f799d5c5fdeb352a4c
amir-skoric <amirskoric@outlook.dk>
2023-04-18 17:39:06 +0200

    change: router controller

--- database/schemas/countriesCollection.js
+++ database/schemas/countriesCollection.js
@@ -2,20 +2,21 @@ const mongoose = require('mongoose');
 
 //city Schema
 const schCountry = new mongoose.Schema( {
-    name: {
-        type: String,
-        required: true
-    },
-    continent: {
-        type: String,
-        required: true
-    },
-    region: {
-        type: String
-    },
-    code: {
-        type: String
-    }
+    code: String,
+    name: String,
+    continent: String,
+    region: String,
+    surfacearea: Number,
+    indepyear: Number,
+    population: Number,
+    lifeexpectancy: Number,
+    gnp: Number,
+    gnpold: Number,
+    localname: String,
+    governmentform: String,
+    headofstate: String,
+    capital: Number,
+    code2: String
 })
 
 const cityCollection = new mongoose.model("country", schCountry, "country");
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const con = require('../controllers/controllers')
+const con = require("../controllers/controllers");
+const conw = require("../controllers/controllersworld");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get("/", function (req, res) {
+  res.render("index");
 });
 
+router.post("/register", con.postUser, function (req, res) {
+  res.redirect("/");
+});
 
-router.post('/register', con.postUser, function(req, res, next) {
-  res.redirect('/')
-})
+//
+/*
+ * Setup REST endpoints
+ */
+
+// Getting countries
+router.get("/countries", conw.getCountries, function (req, res) {
+  // variable from middleware
+  res.json({ countries: res.locals.countries });
+});
+
+// Getting cities
+router.get("/cities", conw.getCities, function (req, res) {
+  // variable from middleware
+  res.json({ cities: res.locals.cities });
+});
+
+// Create city
+router.post("/cities", conw.postCity, function (req, res) {
+  res.status(201).json({ message: "city created" });
+});
+
+// Getting city
+router.get("/cities/:city", conw.getCity, function (req, res) {
+  res.json({ city: res.locals.city });
+});
+
+// Update city
+router.patch("/cities/:city", function (req, res) {});
+
+// Delete city
+router.delete("/cities/:city", conw.deleteCity, function (req, res) {});
 
 module.exports = router;
